d5b2ca2c4326fb408037fa54e1df97adba8ce160
Bart de Boer <bartdb@gmail.com>
2015-04-04 22:11:50 +0200

    Lesson 1.2 done

--- src/Unit-1/DoThis/ConsoleReaderActor.cs
+++ src/Unit-1/DoThis/ConsoleReaderActor.cs
@@ -3,37 +3,91 @@ using Akka.Actor;
 
 namespace WinTail
 {
-    /// <summary>
-    /// Actor responsible for reading FROM the console. 
-    /// Also responsible for calling <see cref="ActorSystem.Shutdown"/>.
-    /// </summary>
-    class ConsoleReaderActor : UntypedActor
+  /// <summary>
+  /// Actor responsible for reading FROM the console. 
+  /// Also responsible for calling <see cref="ActorSystem.Shutdown"/>.
+  /// </summary>
+  internal class ConsoleReaderActor : UntypedActor
+  {
+    public const string ExitCommand = "exit";
+    public const string StartCommand = "start";
+    private ActorRef _consoleWriterActor;
+
+    public ConsoleReaderActor(ActorRef consoleWriterActor)
     {
-        public const string ExitCommand = "exit";
-        private ActorRef _consoleWriterActor;
-
-        public ConsoleReaderActor(ActorRef consoleWriterActor)
-        {
-            _consoleWriterActor = consoleWriterActor;
-        }
-
-        protected override void OnReceive(object message)
-        {
-            var read = Console.ReadLine();
-            if (!string.IsNullOrEmpty(read) && String.Equals(read, ExitCommand, StringComparison.OrdinalIgnoreCase))
-            {
-                // shut down the system (acquire handle to system via
-                // this actors context)
-                Context.System.Shutdown();
-                return;
-            }
-
-            // send input to the console writer to process and print
-            // YOU NEED TO FILL IN HERE
-
-            // continue reading messages from the console
-            // YOU NEED TO FILL IN HERE
-        }
-
+      _consoleWriterActor = consoleWriterActor;
     }
+
+    protected override void OnReceive(object message)
+    {
+      if (message.Equals(StartCommand))
+      {
+        DoPrintInstructions();
+      }
+      else if (message is Messages.InputError)
+      {
+        _consoleWriterActor.Tell(message as Messages.InputError);
+      }
+
+      GetAndValidateInput();
+    }
+
+    #region Internal methods
+
+    private void DoPrintInstructions()
+    {
+      Console.WriteLine("Write whatever you want into the console!");
+      Console.WriteLine("Some entries will pass validation, and some won't...\n\n");
+      Console.WriteLine("Type 'exit' to quit this application at any time.\n");
+    }
+
+    /// <summary>
+    /// Reads input from console, validates it, then signals appropriate response
+    /// (continue processing, error, success, etc.).
+    /// </summary>
+    private void GetAndValidateInput()
+    {
+      var message = Console.ReadLine();
+      if (string.IsNullOrEmpty(message))
+      {
+        // signal that the user needs to supply an input, as previously
+        // received input was blank
+        Self.Tell(new Messages.NullInputError("No input received."));
+      }
+      else if (String.Equals(message, ExitCommand, StringComparison.OrdinalIgnoreCase))
+      {
+        // shut down the entire actor system (allows the process to exit)
+        Context.System.Shutdown();
+      }
+      else
+      {
+        var valid = IsValid(message);
+        if (valid)
+        {
+          _consoleWriterActor.Tell(new Messages.InputSuccess("Thank you! Message was valid."));
+
+          // continue reading messages from console
+          Self.Tell(new Messages.ContinueProcessing());
+        }
+        else
+        {
+          Self.Tell(new Messages.ValidationError("Invalid: input had odd number of characters."));
+        }
+      }
+    }
+
+    /// <summary>
+    /// Validates <see cref="message"/>.
+    /// Currently says messages are valid if contain even number of characters.
+    /// </summary>
+    /// <param name="message"></param>
+    /// <returns></returns>
+    private static bool IsValid(string message)
+    {
+      var valid = message.Length%2 == 0;
+      return valid;
+    }
+
+    #endregion
+  }
 }
